5f3b7a73cc28e4b49e477261fc6022457422185a
nastasjaavilova <nastasja.avilova@gmail.com>
2021-08-05 21:34:00 +0300

    Add some JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -3,54 +3,67 @@
 // Поля формы
 let inputName = document.querySelector('[name="input_name"]')
 let inputDescription = document.querySelector('[name="input_description"]')
+let placeName = document.querySelector('[name="place_name"]')
+let placeLink = document.querySelector('[name="place_link"]')
 
 // Кнопка редактирования
 let editButton = document.querySelector('.profile__edit-button')
 
 // Кнопка закрытия поп-апа
-let closeButton = document.querySelector('.popup__close-button')
+let closeButtons = document.querySelectorAll('.popup__close-button')
+
+// Кнопка добавления картинки
+let addButton = document.querySelector('.profile__add-button')
 
 // Форма редактирования профиля
-let form = document.querySelector('.form')
+let profileEditForm = document.querySelector('[name="profile-edit-form"]')
+// 
+const cardAddForm = document.querySelector('[name="card-add-form"]')
 
 // Получаем данные пользователя из профиля
 let profileName = document.querySelector('.profile__name')
 let profileDescription = document.querySelector('.profile__description')
 
-// Задаём переменную для поп-апа
-let popup = document.querySelector('.popup')
+// Задаём переменную для поп-апа редактировния
+let popupEdit = document.querySelector('[name="popup_edit"]')
+// Задаём переменную для поп-апа добавления
+let popupAdd = document.querySelector('[name="popup_add"]')
+// Зададим класс для всех поп-апов, чтобы обращаться ко всем сразу
+const allPopups = document.querySelectorAll('.popup')
 
 // И для правила когда он открыт
-let target_class = 'popup_opened'
+let openClass = 'popup_opened'
 
-// Блок запуска поп-апа
+// Общий блок поведения поп-апов
 // --------------------
-  // Переключает видимость поп-апа с редактированием профиля
-function togglePopup() {
-  
-  // Если в поп-апе уже есть нужный класс,
-  if (popup.classList.contains(target_class)) {
-    // Мы его отключаем
-    popup.classList.remove(target_class)
-    console.log('popup closed')
-  } else {
-    // Иначе откроем поп-ап, добавив к нему модификатор
-    popup.classList.add(target_class)
-    console.log('popup opened')
-
-    // Подтягиваем данные профиля в форму
-    inputName.value = profileName.textContent
-    inputDescription.value = profileDescription.textContent
-  }
-
+  // Переключает видимость заданного поп-апа
+function openPopup(popup) {
+  // Откроем поп-ап, добавив к нему модификатор
+  popup.classList.add(openClass)
+  console.log('popup opened')
 }
 
-// Задаём одинаковое действие при клике на кнопки редактирования и закрытия поп-апа
-editButton.onclick = togglePopup
-closeButton.onclick = togglePopup
+// function closePopups() {
+//   // Закрывает все попапы
+//   for (i = 0; i < allPopups.length; i++) {
+//     // Пробегается по поп-апам и закрывает их
+//     allPopups[i].classList.remove(openClass)
+//   }
+//   console.log('popups closed')
+// }
 
-// Блок сохранения профиля
+// Блок поп-апа профиля
 // --------------------
+// Открывает поп-ап редактирования профиля
+function openEditPopup() {
+// Открываем попап
+openPopup(popupEdit)
+
+// Подтягиваем данные профиля в форму
+inputName.value = profileName.textContent
+inputDescription.value = profileDescription.textContent
+}
+// Сохраняем профиль
 function saveProfile(evt) {
   // Останавливаем стандартный submit
   evt.preventDefault()
@@ -61,9 +74,101 @@ function saveProfile(evt) {
 
   console.log('profile saved')
 
-  // Закрываем поп-ап
-  togglePopup()
+  closeParentPopup(evt)
 }
 
+// Что будет, когда мы нажимаем кнопки
+editButton.addEventListener('click', openEditPopup)
 // Задаём действие кнопке сохранения
-form.addEventListener('submit', saveProfile)
+profileEditForm.addEventListener('submit', saveProfile)
+
+
+function closeParentPopup(evt) {
+  // Берём нажатую кнопку и закрываем её родительский поп-ап
+  evt.target.closest('.popup').classList.remove('popup_opened')
+}
+
+// Всем кнопкам закрытия задаём действие закрытия
+document.querySelectorAll('.popup__close-button').forEach(button => {
+  button.addEventListener('click', closeParentPopup)})
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+cards = document.querySelector('.elements')
+
+function deleteCard(evt) {
+  card = evt.target.closest('.elements__card') // parent
+  cards.removeChild(card)
+  console.log('card removed')
+}
+
+function createCard(data) {
+  // Находим шаблон
+  const card_template = document.querySelector('[name="card"]').content;
+  // Клонируем его
+  const card = card_template.querySelector('.elements__card').cloneNode(true)
+  
+  // Задаём новое наполнение
+  card.querySelector('.elements__title').textContent = data.name
+  card.querySelector('.elements__image').setAttribute('src', data.link) // img src
+  card.querySelector('.elements__image').setAttribute('alt', data.name)
+
+  card.querySelector('.elements__trash').addEventListener('click', deleteCard)
+  cards.prepend(card)
+  console.log('card added')
+}
+
+initialCards.forEach(cardData => createCard(cardData))
+
+
+
+addCardNameField = popupAdd.querySelector('[name="place_name"]')
+addCardLinkField = popupAdd.querySelector('[name="place_link"]')
+// Блок поп-апа добавления картинки
+// --------------------
+function openAddPopup() {
+  openPopup(popupAdd)
+}
+
+addButton.onclick = openAddPopup
+
+function addCardFromPopup(evt) {
+  evt.preventDefault()
+
+  // Присваиваем профилю значения из формы
+  cardData = {name: addCardNameField.value, link: addCardLinkField.value}
+
+  createCard(cardData)
+
+  console.log('card added')
+
+  closeParentPopup(evt)
+}
+
+cardAddForm.addEventListener('submit', addCardFromPopup)
